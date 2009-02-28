da1dd37b97bb356cd2526c32943b66593a667f12
jenseralmeida <jenseralmeida@4e7f3b2c-d4a3-11dd-9042-6d746b85d38b>
2009-02-28 04:55:46 +0000

    Moving the more recent production code (v0.5) to the trunk

---
+++ code/TaikodomStats.DataSL/Skill.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * Criado por SharpDevelop.
+ * Usuário: Jenser
+ * Data: 28/12/2008
+ * Hora: 23:34
+ * 
+ * Para alterar este modelo use Ferramentas | Opções | Codificação | Editar Cabeçalhos Padrão.
+ */
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TaikodomStats.DataSL
+{
+    /// <summary>
+    /// Description of Skill.
+    /// </summary>
+    public class Skill
+    {
+        private readonly List<Career> careers;
+        //private readonly IDictionary<short, SkillPoint> skillPoints;
+
+        public Skill(string name)
+        {
+            Name = name;
+            careers = new List<Career>();
+#warning nao consegui substituir isto
+            //skillPoints = new SortedList<short, SkillPoint>();
+        }
+
+        public string Name { get; set; }
+
+        public Career[] Careers
+        {
+            get { return careers.ToArray(); }
+        }
+
+        /// <summary>
+        /// Add Career to the list of contained careers of the skill
+        /// </summary>
+        /// <param name="career"></param>
+        internal void AddToCareer(Career career)
+        {
+            if (!ContainedInCareer(career))
+            {
+                careers.Add(career);
+            }
+        }
+
+        public bool ContainedInCareer(Career career)
+        {
+            IEnumerable<Career> q = from c in careers where c == career select c;
+            bool contains = q.Count() > 0;
+            return contains;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
